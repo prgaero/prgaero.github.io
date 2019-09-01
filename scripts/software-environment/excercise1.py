cbd15e2009bbd9681bda2fae7506177d06ff2992
Chahat Deep Singh <chahatdeepsingh@gmail.com>
2019-09-01 19:30:41 -0400

    Create excercise1.py

---
+++ scripts/software-environment/excercise1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+###############################################################################
+######################## Multiplication of two matrices #######################
+###############################################################################
+
+############### Import libraries that you need
+# eg: import cv2 or import sys
+
+
+class operation:
+	def __init__(self, A,B):
+		# use the arguments A and B(matrices)
+
+		# take proper argument for the functions below
+	def calculate_product():
+		# calculate product of matrices A and B
+		return prod 
+
+def main():
+
+	# Create an object of class operation and 
+	# initialize it with matrices A and B
+	
+	# call the funciton to calculate product
+
+	#  print output to view on terminal
+
+
+# below is a general boiler plate to run the main function
+if __name__ == '__main__':
+	main()
